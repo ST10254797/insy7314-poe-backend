81e3898b94a6631897563a4a60622dd2e5bcc196
Keyur Ramdeen Singh <ST10266914@vcconnect.edu.za>
2025-10-08 16:23:28 +0200

    Adding validation ofr accNumber

--- Middleware/validateInput.js
+++ Middleware/validateInput.js
@@ -2,6 +2,7 @@ module.exports = (req, res, next) => {
     const { fullName, IDNumber, AccNumber, userName, password } = req.body;
     const regexName = /^[a-zA-Z\s]+$/;
     const regexID = /^\d{13}$/;
+    const regexAcc = /^\d{8,12}$/; 
     const regexUser = /^[a-zA-Z0-9_]{4,20}$/;
     const regexPass = /^[A-Za-z0-9!@#$%^&*]{6,20}$/;
 
@@ -9,8 +10,12 @@ module.exports = (req, res, next) => {
         return res.status(400).json({ message: 'All fields are required' });
     }
 
-    if (!regexName.test(fullName) || !regexID.test(IDNumber) 
-        || isNaN(AccNumber) || !regexUser.test(userName) || !regexPass.test(password)) {
+    if (!regexName.test(fullName) || 
+        !regexID.test(IDNumber) || 
+        !regexAcc.test(AccNumber.toString()) || 
+        !regexUser.test(userName) || 
+        !regexPass.test(password)) 
+    {
         return res.status(400).json({ message: 'Invalid input format' });
     }
 
